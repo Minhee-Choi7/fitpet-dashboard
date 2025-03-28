6e43885099c2e5636055467666f6b50946b59581
Minhee-Choi7 <minhee.choi@fitpet.co.kr>
2025-03-28 18:02:47 +0900

    수수정

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,6 +1,6 @@
 
 import React, { useState } from "react";
-import { Card, CardContent } from "../components/ui/Card";
+import { Card, CardContent } from "./components/ui/Card";
 import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer, BarChart, Bar, PieChart, Pie, Cell } from "recharts";
 
 const monthlyData = [
